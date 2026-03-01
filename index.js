eb6aa2be21d7ae140a29b1217b8b85ee7ef8b0ca
goutampatil91 <goutampatil3335@gmail.com>
2026-03-01 15:39:06 +0530

    Fix port handling and add github route response

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
+
+const githubData = {
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://hitesh.ai",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 119,
+  "public_gists": 5,
+  "followers": 55260,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2026-02-05T17:51:40Z"
+}
 
 app.get('/', (req, res) => {  // get request
     res.send('Hello World!')
@@ -21,6 +57,11 @@ app.get('/youtube', (req, res) => {
     res.send('<h2>chai aur code</h2>')
 })
 
-app.listen(process.env.PORT, () => {
+app.get('/github', (req, res) => {
+    // return the hardcoded profile data for now
+    res.json(githubData)
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
